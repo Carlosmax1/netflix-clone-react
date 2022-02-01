03a40aca0dfb98fb789e024c5ae04cafbc1d6127
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2022-02-01 11:33:28 -0300

    Inicio da criacao do cabecalho, e filme me destaque

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,8 +1,74 @@
 import react from "react";
 import './index.css'
+import Movies from "../../movies";
+import { useEffect, useState } from "react";
 
-export default function Cabecalho(){
-    return(
-        <h1>Cabecalho</h1>
-    )
+
+export default function Cabecalho() {
+
+	const [movieList, setMovieList] = useState([]);
+	const [destaque, setDestaque] = useState(null);
+	const [generos, setGeneros] = useState([]);
+
+	useEffect(() => {
+		const moveis = async () => {
+			let list = await Movies();
+			setMovieList(list);
+		}
+
+		moveis();
+
+	}, []);
+
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setDestaque(movieList[2].items.results)
+		}
+	}, [movieList])
+
+	const number = Math.floor(Math.random() * 20);
+	// console.log(destaque)
+
+
+	// useEffect(() =>{
+	// 	if(destaque.length > 0){
+	// 		setGeneros(destaque[number].genre_ids)
+	// 	}
+	// },[destaque]);
+
+	// console.log(generos);
+
+	return (
+		<>
+			{destaque !== null && (
+				<header className="cabecalho" style={{
+					backgroundSize: 'cover',
+					backgroundPositon: 'center',
+					backgroundImage: `url(https://image.tmdb.org/t/p/original/${destaque[number].backdrop_path})`
+				}}>
+					<div className="vertical">
+						<div className="horizontal">
+							<div className="container">
+								<div className="info-filmes">
+									<div className="titulo-filme">{destaque[number].title}</div>
+									<div className="pontucao-filme">{destaque[number].vote_average} pontos</div>
+									<div className="ano-filme">{destaque[number].release_date}</div>
+									<div className="temporadas">a</div>
+									<div className="descricao-filme">{destaque[number].overview}</div>
+									<div className="genero">a</div>
+								</div>
+								<div className="container-logo">
+									<img className="logo" src='https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456'></img>
+								</div>
+								<div className="netflix-avatar-container">
+									<img className="netflix-avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+			)}
+		</>
+
+	)
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,14 +1,19 @@
 import react from "react";
 import { useState, useEffect } from "react";
 import Movies from "../../movies";
+import Cabecalho from "../Cabecalho";
 import './index.css';
 
 export default function Main() {
 
-	const [movieList, setMovieList] = useState([]);
-	const [originais, setOriginais] = useState(null);
-	const [recomendados, setRecomendados] = useState(null);
-
+	const [movieList, setMovieList] = useState([]); // Lista completa de filmes
+	const [originais, setOriginais] = useState(null); // Originais Netflix
+	const [recomendados, setRecomendados] = useState(null); // Recomendados
+	const [acao, setAcao] = useState(null); // Filme de ação
+	const [comedia, setComedia] = useState(null); // Comedia 
+	const [terror, setTerror] = useState(null); // Terror
+	const [romance, setRomance] = useState(null); // Romance
+	const [documentario, setDocumentario] = useState(null); // Documentarios
 
 	useEffect(() => {
 		const moveis = async () => {
@@ -34,9 +39,44 @@ export default function Main() {
 
 	}, [movieList])
 
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setAcao(movieList[3].items.results)
+		}
+
+	}, [movieList])
+
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setComedia(movieList[4].items.results)
+		}
+	}, [movieList])
+
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setTerror(movieList[5].items.results)
+		}
+
+	}, [movieList])
+
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setRomance(movieList[6].items.results)
+		}
+
+	}, [movieList])
+
+	useEffect(() => {
+		if (movieList.length > 0) {
+			setDocumentario(movieList[7].items.results);
+		}
+
+	}, [movieList])
+
 	return (
 
 		<>
+			<Cabecalho />
 			<main className="originais-container">
 				<h1 className="titulo-originais">Originais Netflix</h1>
 				<section className="originais">
@@ -56,7 +96,51 @@ export default function Main() {
 					</> :
 						<div>oi2</div>}
 				</section>
-
+				<h1 className="titulo-acao">Ação</h1>
+				<section className="filmes-acao">
+					{acao !== null ? <>
+						{acao.map((item, key) =>
+							<img key={key} className="poster-filme" src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}></img>
+						)}
+					</> :
+						<div>oi2</div>}
+				</section>
+				<h1 className="titulo-comedia">Comédia</h1>
+				<section className="filmes-comedia">
+					{comedia !== null ? <>
+						{comedia.map((item, key) =>
+							<img key={key} className="poster-filme" src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}></img>
+						)}
+					</> :
+						<div>oi2</div>}
+				</section>
+				<h1 className="titulo-terror">Terror</h1>
+				<section className="filmes-terror">
+					{terror !== null ? <>
+						{terror.map((item, key) =>
+							<img key={key} className="poster-filme" src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}></img>
+						)}
+					</> :
+						<div>oi2</div>}
+				</section>
+				<h1 className="titulo-romance">Romance</h1>
+				<section className="filmes-romance">
+					{romance !== null ? <>
+						{romance.map((item, key) =>
+							<img key={key} className="poster-filme" src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}></img>
+						)}
+					</> :
+						<div>oi2</div>}
+				</section>
+				<h1 className="titulo-documentarios">Documentários</h1>
+				<section className="documentarios">
+					{documentario !== null ? <>
+						{documentario.map((item, key) =>
+							<img key={key} className="poster-filme" src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}></img>
+						)}
+					</> :
+						<div>oi2</div>}
+				</section>
 			</main>
 		</>
 	)
