9f990dee555f4626d941be0552759dfe228559d9
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2022-02-02 17:05:13 -0300

    Estilização do filme me destaque no cabeÃçalho

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -3,6 +3,8 @@ import './index.css'
 import Movies from "../../movies";
 import getMovieInfo from "../../tv_infos";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function Cabecalho() {
 
@@ -36,15 +38,18 @@ export default function Cabecalho() {
   useEffect(() => {
     if (destaque !== null) {
       const generosLista = destaque.genres
+      console.log(generosLista);
+      const generosNomes = []
       for (let i = 0; i < generosLista.length; i++) {
-        setGeneros(generosLista[i].name);
+        generosNomes.push(generosLista[i].name);
       }
+      setGeneros(generosNomes);
       const anoDeLancamento = destaque.first_air_date
-      setAno(anoDeLancamento.slice(0,4));
+      setAno(anoDeLancamento.slice(0, 4));
     }
   }, [destaque])
 
-  console.log(destaque);
+  // console.log(generos);
 
   return (
     <>
@@ -66,11 +71,17 @@ export default function Cabecalho() {
               </div>
               <div className="info-filmes">
                 <div className="titulo-filme">{destaque.name}</div>
-                <div className="pontucao-filme">{destaque.vote_average} pontos</div>
-                <div className="ano-filme">{ano}</div>
-                <div className="temporadas">{destaque.number_of_seasons}</div>
+                <div className="pat">
+                  <div className="pontucao-filme">{destaque.vote_average} pontos</div>
+                  <div className="ano-filme">{ano}</div>
+                  <div className="temporadas">{destaque.number_of_seasons} temporada(s)</div>
+                </div>
                 <div className="descricao-filme">{destaque.overview}</div>
-                <div className="generos">{generos}</div>
+                <div className="botoes">
+                  <button className="botao-assitir"><FontAwesomeIcon icon={faPlay} size="xs"></FontAwesomeIcon> Assitir</button>
+                  <button className="botao-minhaLista">+ Minha lista</button>
+                </div>
+                <div className="generos">Gênero(s): {generos.join(', ')}</div>
               </div>
             </div>
           </div>
