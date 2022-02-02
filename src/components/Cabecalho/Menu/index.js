08ab7c5d2f5244f580bc97a7bbc181212420b344
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2022-02-02 18:18:01 -0300

    adição dos icones, nonotificações e pesquisar

--- src/components/Cabecalho/Menu/index.js
+++ src/components/Cabecalho/Menu/index.js
@@ -1,4 +1,6 @@
 import react from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faSearch} from "@fortawesome/free-solid-svg-icons";
 import './index.css'
 
 export default function Menu() {
@@ -6,6 +8,10 @@ export default function Menu() {
 		<div className="menu">
 			<div className="container-logo">
 				<img className="logo" src='https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png'></img>
+			</div>
+			<div className="icones">
+				<FontAwesomeIcon className="icone" size='lg' icon={faSearch}></FontAwesomeIcon>
+				<FontAwesomeIcon className="icone" size='lg' icon={faBell}></FontAwesomeIcon>
 			</div>
 			<div className="netflix-avatar-container">
 				<img className="netflix-avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
